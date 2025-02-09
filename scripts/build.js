d5d2a65439c0180280cb568df6fe67fb0587288e
jdy8739 <jdy8739@naver.com>
2025-02-09 22:55:41 +0900

    feat: init build process by copying html files

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+fs.mkdirSync('dist');
+
+const files = fs.readdirSync('pages');
+
+for (const file of files) {
+    const fileName = `pages/${file}`;
+
+    if (file === 'index.html') {
+        fs.copyFileSync(fileName, `dist/${file}`);
+    } else {
+        const dirName = file.split('.')[0];
+
+        const path = `dist/${dirName}`;
+
+        fs.mkdirSync(path);
+
+        fs.copyFileSync(fileName, `${path}/${file}`);
+    }
+}
